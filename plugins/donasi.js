e1820398c143e1432f6e80e36904fc208da35624
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-12 22:45:39 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Telkomsel [08133533848]
+│ • Gopay  [085785694474]
+│ • Dana  [08133533848]
+│ • Follow Ig jg bisa  [https://Instagram.com/nan_sakrin]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/66855048169
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_

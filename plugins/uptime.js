3885a01f12881ac5e45c0e227254f799f5549dd5
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-12 22:51:56 +0700

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -23,7 +23,7 @@ var seconds = Math.floor(seconds % 60);
 return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
 }
 runtime = process.uptime()
-					teks = `The.sad.boy01 ×͜×`// \n\n${kyun(runtime)}`
+					teks = `Nan Sakkarin`// \n\n${kyun(runtime)}`
 					run = `${kyun(runtime)}`
 					var itsme = `0@s.whatsapp.net`
 					var split = `teks`

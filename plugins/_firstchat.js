b14cc6a4125b3fd1120b00ae8dd5f086b31291ff
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-14 06:35:39 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -11,7 +11,7 @@ handler.all = async function (m) {
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
+${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?\nSaya adalah robot WhatsApp, jika anda chat saya\nSaya tidak akan merespon sedikitpun\ndan jika merasa terganggu, mohon di blokir saja, sekian'}
 `.trim(), wm, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '.owner' : '.menu', m)
     user.pc = new Date * 1
 }
